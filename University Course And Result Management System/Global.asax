36f3f8e856ee6a2b0089c5da30da0c4ccd09a9bd
Abiduddin <abidakash456@gmail.com>
2018-11-13 19:15:53 +0600

    version-1
    initial state

---
+++ University Course And Result Management System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="University_Course_And_Result_Management_System.MvcApplication" Language="C#" %>
---
+++ University Course And Result Management System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(University_Course_And_Result_Management_System.Startup))]
+namespace University_Course_And_Result_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
